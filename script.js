ae8eef2e10d8234478f0f2ea2e4666896b7320e8
Harinidevi26 <harinidevi467@gmail.com>
2024-12-23 14:35:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript Program to greet the user
+
+// Ask for the user's name
+let userName = prompt("What's your name?");
+
+// Display a greeting message
+alert("Hello, " + userName + "! Welcome to the world of JavaScript!");
